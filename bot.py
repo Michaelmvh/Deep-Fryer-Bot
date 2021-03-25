01c17747832c3b904eba247088576e537ee9a126
michaelmvh <mmvandenheuv@wisc.edu>
2021-03-25 15:46:21 -0500

    Added meme and bottomtext commands

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-from deepfry import deepFryImage
+from deepfry import deepFryImage, bottomTextImage, memeImage
 from io import BytesIO
 
 load_dotenv()
@@ -20,6 +20,36 @@ async def deepfrycommand(context, arg):
     #Converts our image object to Bytes to discord file
     with BytesIO() as image_binary:
         deepFryImage(arg).save(image_binary, 'PNG')
+        image_binary.seek(0)
+        await context.send(file=discord.File(fp=image_binary, filename='deepfried.png'))
+
+        ## if deepfry for message
+
+        ## if deepfry for quote 
+        
+        ## else deepfry previous image
+
+@client.command(name='bottomtext', pass_context=True)
+async def deepfrycommand(context, arg):
+    print(arg)
+    #Converts our image object to Bytes to discord file
+    with BytesIO() as image_binary:
+        bottomtext(arg).save(image_binary, 'PNG')
+        image_binary.seek(0)
+        await context.send(file=discord.File(fp=image_binary, filename='deepfried.png'))
+
+        ## if deepfry for message
+
+        ## if deepfry for quote 
+        
+        ## else deepfry previous image
+
+@client.command(name='meme', pass_context=True)
+async def deepfrycommand(context, arg):
+    print(arg)
+    #Converts our image object to Bytes to discord file
+    with BytesIO() as image_binary:
+        memeImage(arg).save(image_binary, 'PNG')
         image_binary.seek(0)
         await context.send(file=discord.File(fp=image_binary, filename='deepfried.png'))
 
--- deepfry.py
+++ deepfry.py
@@ -1,22 +1,30 @@
-from PIL import Image, ImageEnhance  
+from PIL import Image, ImageEnhance, ImageDraw, ImageFont
 import requests
 from io import BytesIO                                                              
 
-def deepFryImage(imageURL):
+def imageToLink(img):
+    if isinstance(img, Image.Image):
+        return img
+    elif not isinstance(img, str):
+        print("not valid type passed")
+        return
     # Import Image from passed URL
     try:
-        response = requests.get(imageURL)
-        img = Image.open(BytesIO(response.content))                                                                 
+        response = requests.get(img)
+        img = Image.open(BytesIO(response.content))                                                         
     except:
         print("error parsing link")
         return
+    return img
 
+def deepFryImage(img):
+    img = imageToLink(img)
     # Saturation
-    #enhancer = ImageEnhance.Color(img)
-    #res = enhancer.enhance(2.0)    
+    enhancer = ImageEnhance.Color(img)
+    res = enhancer.enhance(2.0)    
 
     # Sharpen 
-    enhancer = ImageEnhance.Sharpness(img)
+    enhancer = ImageEnhance.Sharpness(res)
     res = enhancer.enhance(100) 
 
     # Contrast
@@ -27,5 +35,27 @@ def deepFryImage(imageURL):
     #res.save('result.png')
     return res
 
+def bottomTextImage(img):
+    img = imageToLink(img)
+    W, H = img.size
+
+    text = "Bottom Text"
+    font = ImageFont.truetype(
+        font="unicode.impact.ttf",
+        size=350,
+        encoding='unic')
+    draw = ImageDraw.Draw(img)
+    w, h = draw.textsize(text, font=font)
+
+    stroke_color = (0, 0, 0)
+    fill_color = (255, 255, 255)
+
+    draw.text(((W-w)/2, int((H-h)*.99)), text, font=font, fill=fill_color, stroke_width=2, stroke_fill=stroke_color)
+    return img
+
+def memeImage(img):
+    img = imageToLink(img)
+    return bottomTextImage(deepFryImage(img))
+
 ## Example of calling this function
-#deepFryImage("https://www.salton.com/wp-content/uploads/2016/04/DF1233_2-1.jpg")
+meme("https://www.salton.com/wp-content/uploads/2016/04/DF1233_2-1.jpg").save('result.png')
